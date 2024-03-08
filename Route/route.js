610dbb6ff7990a692a18cda846d140c4730c1ecf
RoySuzon <goutomroy770@gmail.com>
2024-03-08 22:27:25 +0600

    delete api update

--- Route/route.js
+++ Route/route.js
@@ -1,60 +1,59 @@
-const express = require('express');
-const { Person } = require('../Schema/schema');
+const express = require("express");
+const { Person } = require("../Schema/schema");
 
 const route = express.Router();
 
 route.get("/login", (req, res) => {
-    res.send("LOGIN");
-})
-
-route.post('/update/:id', express.urlencoded({ extended: false }), express.json(), async function (req, res) {
-    const { id } = req.params;
-    const { title } = req.body;
-
-    try {
-        await Person.findByIdAndUpdate(id, { $set: { title: title } });
-        res.send(await Person.findById(id));
-    } catch (error) {
-        res.status(404).send(error.message);
-    }
-});
-route.get('/delete/:id', async function (req, res) {
-    const { id } = req.params;
-
-    await Person.findByIdAndDelete(id)
-    try {
-        res.send(true);
-    } catch (error) {
-        res.status(404).send(error.message);
-    }
+  res.send("LOGIN");
 });
 
-route.post('/post', function (req, res) {
+route.post("/update/:id", async function (req, res) {
+  const { id } = req.params;
+  const { title } = req.body;
 
-    const { title, author, body } = req.body;
-    const person = new Person({ title: title, author: author, body: body });
-    if (title != null && author != null && body != null) {
-        person.save();
-        res.status(202).send({
-            status: true,
-            person
-        });
-    }
-    else {
-        res.status(404).send({ status: false })
-    }
+  try {
+    await Person.findByIdAndUpdate(id, { $set: { title: title } });
+    res.send({
+      status: true,
+    });
+  } catch (error) {
+    res.status(404).send({ status: false });
+  }
+});
+route.get("/delete/:id", async function (req, res) {
+  const { id } = req.params;
+
+  await Person.findByIdAndDelete(id);
+  try {
+    res.send({
+      status: true,
+    });
+  } catch (error) {
+    res.status(404).send({ status: false });
+  }
 });
 
+route.post("/post", function (req, res) {
+  const { title, author, body } = req.body;
+  const person = new Person({ title: title, author: author, body: body });
+  if (title != null && author != null && body != null) {
+    person.save();
+    res.status(202).send({
+      status: true,
+      person,
+    });
+  } else {
+    res.status(404).send({ status: false });
+  }
+});
 
 route.get("/user", async (req, res) => {
+  const person = await Person.find();
 
-    const person = await Person.find();
-
-    res.send({
-        status: true,
-        users: person
-    });
+  res.send({
+    status: true,
+    users: person,
+  });
 });
-
 
 module.exports = route;
